997bb15a0cf630515a48385ef12be86bf10c2b36
SUSHIL KUMAR <115349532+mrsushilkumar@users.noreply.github.com>
2024-04-30 13:51:05 +0530

    slf
    sldfl a

--- farm_expense_mangement_app/lib/main.dart
+++ farm_expense_mangement_app/lib/main.dart
@@ -4,10 +4,12 @@ import 'package:farm_expense_mangement_app/screens/wrappers/wrapperhome.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   // FirebaseAuth.instance.createUserWithEmailAndPassword(email: '2021csb1136@iitrpr.ac.in', password: 'iit@123#');
   runApp(const MyApp());
